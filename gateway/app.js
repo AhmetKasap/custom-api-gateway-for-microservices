4e7b462b6669885d01e4929ad575beb4c9bc3e54
ahmetkasap <aahmetkasap@gmail.com>
2024-08-09 23:18:52 +0300

    security such as ratelimit, cors and helmet etc added

--- gateway/app.js
+++ gateway/app.js
@@ -1,19 +1,36 @@
-require('express-async-errors');
-
+require('express-async-errors')
 const express = require('express')
 const app = express()
 const checkToken = require('./token/checkToken')
+const cors = require('cors')
+const helmet = require("helmet")
+const morgan = require("morgan")
 
+
+
+//! SECURITY
+app.use(helmet()) 
+app.use(morgan("combined"))
+app.disable("x-powered-by")
+
+const limit = require('./security/rateLimit')
+app.use('/api-gateway/v1/*', limit)
+
+const corsOptions = require('./security/cors')
+//app.use(cors(corsOptions))
+
+
+//!SERVICES
+const gatewayPath="/api-gateway/v1"
 
 const productProxy = require('./services/product')
-app.use('/api-gateway/v1/product', checkToken, productProxy)
-
+app.use(`${gatewayPath}/product`, checkToken, productProxy)
 
 const authProxy = require('./services/auth')
-app.use('/api-gateway/v1/auth/*', authProxy)
+app.use(`${gatewayPath}/auth/*`, authProxy)
 
 
-
+//! Errorhandler
 const errorHandler = require('./Exceptions/errorHandler')
 app.use(errorHandler)
 
